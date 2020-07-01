4d7c3fef1ac7adc15efd4dca5f5e1e02fb98ef58
pedobearearn <pedobearearn@gmail.com>
2020-07-01 10:32:33 +0700

    [Added Init]

--- laravel/run.sh
+++ laravel/run.sh
@@ -1,10 +1,10 @@
 #/bin/sh
 docker-compose down
 docker-compose up -d --build
-docker-compose exec funding_main php artisan migrate:refresh
-docker-compose exec funding_main php artisan passport:install --force
-docker-compose exec funding_main php artisan passport:keys --force
-docker-compose exec funding_main composer dump-autoload
-docker-compose exec funding_main php artisan db:seed
-docker-compose exec funding_main php artisan key:generate
-docker-compose exec funding_main php artisan config:clear
+docker-compose exec main php artisan migrate:refresh
+docker-compose exec main php artisan passport:install --force
+docker-compose exec main php artisan passport:keys --force
+docker-compose exec main composer dump-autoload
+docker-compose exec main php artisan db:seed
+docker-compose exec main php artisan key:generate
+docker-compose exec main php artisan config:clear
